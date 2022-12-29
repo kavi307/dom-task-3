d4061f3cd9055789303e2befb55f1f3f46e61899
kavi307 <ajkavitha7@gmail.com>
2022-12-29 19:53:44 +0530

    calculator

---
+++ main.js
@@ -0,0 +1,12 @@
+function plus(){
+    let number1=parseInt(document.getElementById("number1").value);
+    let number2=parseInt(document.getElementById("number2").value);
+    var total =number1+number2; 
+    let result=parseInt(document.getElementById("result").value=total);
+}
+function minus(){
+    let number1=parseInt(document.getElementById("number1").value);
+    let number2=parseInt(document.getElementById("number2").value);
+    var total =number1-number2; 
+    let result=parseInt(document.getElementById("result").value=total);
+}
